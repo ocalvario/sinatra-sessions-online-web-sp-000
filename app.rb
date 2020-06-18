274e38fb2dbe9729c8b680c6c953fcae1e0c5385
Oswald Calvario <oswald.calvario@gmail.com>
2020-06-18 21:07:40 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -39,6 +39,7 @@ class App < Sinatra::Base
   get '/set_session' do
     #set session id here
 
+    session[:id] = 1
     if session[:id] == 1
       # "Session ID set. It's currently set to #{session[:id]}."
       redirect '/fetch_session_id'
@@ -46,6 +47,7 @@ class App < Sinatra::Base
       "Session ID has not been set!"
     end
   end
+
 
   get '/fetch_session_id' do
     "You did it! session[:id] value: #{session[:id]}.\nNow, clear the session in the '/logout' route.\nSee the readme for further instructions.\nThen, navigate to the '/logout' path."
